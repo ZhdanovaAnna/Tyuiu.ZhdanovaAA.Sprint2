aef108bd595a6ef68ea791b582370d0a94e8e8f8
ZhdanovaAnna <annaone01@mail.ru>
2023-10-02 16:50:13 +0500

    Task1

---
+++ Tyuiu.ZhdanovaAA.Sprint2.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZhdanovaAA.Sprint2.Task1.V26.Lib;
+
+namespace Tyuiu.ZhdanovaAA.Sprint2.Task1.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+            int a = 654, b = 671, c = 874, d = 137;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, true, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZhdanovaAA.Sprint2.Task1.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhdanovaAA.Sprint2.Task1.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhdanovaAA.Sprint2.Task1.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14362efa-c7cb-4522-b45d-be8f8958dc5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
